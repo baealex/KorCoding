a305df75423c025e94231362715c0a16ee5a3959
Jino Bae <im@baejino.com>
2019-09-20 20:45:07 +0900

    [특징] 자연스러운 어구 구사

--- korcoding.h
+++ korcoding.h
@@ -15,7 +15,17 @@
 
 #define 진입점 main
 
+#define 또는 ||
+#define 그리고 &&
+
+#define 증가 ++
+#define 감소 --
+
+#define 포인터 *
+#define 참조 &
+
 #define 만약 if
+#define 만약에 if
 #define 혹은 else if
 #define 아니면 else
 #define 반복 for
@@ -24,7 +34,13 @@
 #define 정의 typedef
 #define 구조 struct
 
+#define 나의 this
+#define 이것의 this
+
+#define 객체 class
+#define 객체인 class
 #define 클래스 class
+#define 클래스인 class
 #define 공개 public
 #define 숨김 private
 #define 보호 protected
@@ -35,23 +51,54 @@
 #define 삭제 delete
 
 #define 참 true
+#define 진실 true
 #define 거짓 false
+
+typedef int 정수형을반환하는;
+typedef float 실수형을반환하는;
+typedef char 문자형을반환하는;
+
+typedef int 정수를반환하는;
+typedef float 실수를반환하는;
+typedef char 문자를반환하는;
+typedef void 반환하지않는;
+typedef void 반환없음;
+
+typedef int 정수변수인;
+typedef float 실수변수인;
+typedef char 문자변수인;
+
+typedef int 정수형인;
+typedef float 실수형인;
+typedef char 문자형인;
+typedef void 비어있는;
+typedef void 비어있음;
+
+typedef int 정수인;
+typedef float 실수인;
+typedef char 문자인;
+
+typedef int 정수변수;
+typedef float 실수변수;
+typedef char 문자변수;
 
 typedef int 정수;
 typedef float 실수;
 typedef char 문자;
 typedef void 공백;
 
-class 기본입출력
+typedef bool 진실혹은거짓;
+
+class JinIO
 {
 public:
 	template <typename type>
-	기본입출력 출력(type a)
+	JinIO 출력(type a)
 	{
 		std::cout << a;
 		return *this;
 	}
-	기본입출력 새행()
+	JinIO 새행()
 	{
 		std::cout << std::endl;
 		return *this;
@@ -63,13 +110,13 @@ public:
 	}
 };
 
-class 출력옵션
+class JinPrint
 {
 private:
-	기본입출력 &ref;
+	JinIO &ref;
 public:
-	출력옵션(기본입출력 &r) : ref(r) {};
-	기본입출력 진수변환(int x = 10)
+	JinPrint(JinIO &r) : ref(r) {};
+	JinIO 진수변환(int x = 10)
 	{
 		using namespace std;
 		switch (x)
@@ -86,7 +133,7 @@ public:
 		}
 		return ref;
 	}
-	inline 기본입출력 소수점(int x)
+	inline JinIO 소수점(int x)
 	{
 		using namespace std;
 		cout.precision(x);
@@ -94,7 +141,7 @@ public:
 	}
 };
 
-class 시스템클래스
+class JinSystem
 {
 public:
 	inline void 정지()
@@ -111,10 +158,10 @@ public:
 	}
 };
 
-class 수학클래스
+class JinMath
 {
 public:
-	수학클래스()
+	JinMath()
 	{
 		srand(time(NULL));
 	}
@@ -142,8 +189,7 @@ public:
 	}
 };
 
-/*
-class 문자열클래스 {
+class JinString {
 public:
 	char** 자르기(char* tmp, char ch)
 	{
@@ -190,7 +236,6 @@ template <typename T>
 int 길이(T *len = nullptr)
 {
 	int count = 0;
-
 	while (1) {
 		if (*(len + count) == NULL) {
 			break;
@@ -201,10 +246,10 @@ int 길이(T *len = nullptr)
 }
 // */
 
-기본입출력 입출력;
-출력옵션 출력(입출력);
-// 문자열클래스 문자열;
-시스템클래스 콘솔;
-수학클래스 수학;
+JinIO 입출력;
+JinPrint 출력(입출력);
+JinString 문자열;
+JinSystem 콘솔;
+JinMath 수학;
 
 #endif
